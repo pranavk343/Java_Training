44baa35254aafb45d5135ee47a4dcddbcde9eb19
Pranav Kulkarni <67072785+pranavk343@users.noreply.github.com>
2022-05-20 12:59:26 +0530

    MVC Initial Commit
    Basic introduction to MVC Architecture

---
+++ Day 38 (MVC Architecture)/Day-38_1/src/main/java/com/example/Day/_1/Day381Application.java
@@ -0,0 +1,13 @@
+package com.example.Day._1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day381Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day381Application.class, args);
+	}
+
+}
---
+++ Day 38 (MVC Architecture)/Day-38_1/src/test/java/com/example/Day/_1/Day381ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Day._1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day381ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
