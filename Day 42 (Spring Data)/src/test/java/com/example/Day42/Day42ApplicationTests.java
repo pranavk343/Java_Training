7a87f678f1dbea9cef51fc723d2cde4337bc273c
Pranav Kulkarni <67072785+pranavk343@users.noreply.github.com>
2022-05-25 12:12:06 +0530

    Initial Commit

---
+++ Day 42 (Spring Data)/src/test/java/com/example/Day42/Day42ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Day42;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day42ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
